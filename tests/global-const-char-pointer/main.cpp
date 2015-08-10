74845e15ead2c297cead511dc582278aa73edae3
Sergio Martins <sergio.martins@kdab.com>
2015-08-10 16:51:39 +0100

    global-const-char-pointer: Add unit-test

---
+++ tests/global-const-char-pointer/main.cpp
@@ -0,0 +1,21 @@
+
+
+
+
+static const char * const foo  = "1";
+static const char * foo2 = "2";
+const char * foo3 = "3"; // Warning
+const char * const foo4 = "4";
+static const char foo5[] ="5";
+const char foo5[] ="5";
+
+namespace {
+    const char * foo6 = "6";
+}
+
+void test()
+{
+    const char * foo6 = "6";
+}
+
+
