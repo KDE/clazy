3adf033a5768466ef2b8db8c4ecc3f5ce527e88d
Sergio Martins <sergio.martins@kdab.com>
2015-08-12 20:57:10 +0100

    global-const-char-pointer: Fix unit-test build

--- tests/global-const-char-pointer/main.cpp
+++ tests/global-const-char-pointer/main.cpp
@@ -5,9 +5,10 @@
 static const char * const foo  = "1";
 static const char * foo2 = "2";
 const char * foo3 = "3"; // Warning
+
 const char * const foo4 = "4";
 static const char foo5[] ="5";
-const char foo5[] ="5";
+const char foo51[] ="5";
 
 namespace {
     const char * foo6 = "6";
