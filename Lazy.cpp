da061fd26b6c7922c54b61bff56e7346430ba7c4
Sergio Martins <sergio.martins@kdab.com>
2015-08-26 13:37:53 +0100

    Intruducing the clazy wrapper script.
    Updated README on how to use the tool.

--- Lazy.cpp
+++ Lazy.cpp
@@ -176,7 +176,7 @@ protected:
             // No check specified, check the env variable
             m_checks = CheckManager::instance()->requestedCheckNamesThroughEnv();
             if (m_checks.empty()) {
-                llvm::errs() << "No requested checks! ";
+                llvm::errs() << "No requested checks!\n";
                 //PrintHelp(llvm::errs());
                 return true;
             }
@@ -192,7 +192,7 @@ protected:
         } else if (args.size() == 1) {
             m_checks = CheckManager::instance()->checkNamesForCommaSeparatedString(args[0]);
             if (m_checks.empty()) {
-                llvm::errs() << "No requested checks! ";
+                llvm::errs() << "No requested checks!\n";
                 PrintHelp(llvm::errs());
                 return false;
             }
