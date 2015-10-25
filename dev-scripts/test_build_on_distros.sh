86a48b6fa0239d7e36bf79135cea06bc51e4b1a2
Sergio Martins <smartins@kde.org>
2015-10-25 20:11:46 +0000

    Add script to test build on other distros.
    There are some differences in llvm library naming across distros, so it's useful to test on more distros
    specially after messing with the cmake magic.

---
+++ dev-scripts/test_build_on_distros.sh
@@ -0,0 +1,11 @@
+BRANCH=$1
+J_FLAG=$2
+BUILD_SCRIPT=/usr/bin/build-clazy.sh
+
+if [ -z "$1" ]
+then
+    BRANCH="master"
+fi
+
+docker run -i -t iamsergio/clazy-ubuntu-15.10 sh $BUILD_SCRIPT $BRANCH $J_FLAG && \
+docker run -i -t iamsergio/clazy-opensuse-tumbleweed sh $BUILD_SCRIPT $BRANCH $J_FLAG
