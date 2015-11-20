acd3c1d28e2c53dabfb68ea5565627e3874b4387
Sergio Martins <smartins@kde.org>
2015-11-20 12:18:32 +0000

    old-style-connect: Make it a level2 check instead of level1
    Meaning it doesn't run by default.
    PMF syntax isn't perfect and can introduce bugs.
    Will create a README for it and explain which edge cases you should
    watch out for.

--- checks/oldstyleconnect.cpp
+++ checks/oldstyleconnect.cpp
@@ -459,5 +459,5 @@ vector<FixItHint> OldStyleConnect::fixits(int classification, CallExpr *call)
 }
 
 const char *const s_checkName = "old-style-connect";
-REGISTER_CHECK_WITH_FLAGS(s_checkName, OldStyleConnect, CheckLevel1)
+REGISTER_CHECK_WITH_FLAGS(s_checkName, OldStyleConnect, CheckLevel2)
 REGISTER_FIXIT(FixItConnects, "fix-old-style-connect", s_checkName)
