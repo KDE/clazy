323b03320d7e54408803dfc8c311088666db6397
Sergio Martins <smartins@kde.org>
2015-12-10 21:58:41 +0000

    Revert "Sync FindCLang.cmake and FindLLVM.cmake with kdevelop to fix windows build."
    This reverts commit c62ff367d7352cba01945ec091a85bd79c3aa538.
    
    Reverted because now it breaks archlinux, investigating..

--- cmake/FindClang.cmake
+++ cmake/FindClang.cmake
@@ -5,8 +5,7 @@
 #  CLANG_INCLUDE_DIR           - Where to find Clang includes
 #  CLANG_LIBRARY_DIR           - Where to find Clang libraries
 #
-#  CLANG_LIBCLANG_LIB          - Libclang C library
-#
+#  CLANG_CLANG_LIB             - LibClang library
 #  CLANG_CLANGFRONTEND_LIB     - Clang Frontend Library
 #  CLANG_CLANGDRIVER_LIB       - Clang Driver Library
 #  ...
@@ -16,7 +15,7 @@
 # See http://clang.llvm.org/docs/InternalsManual.html for full list of libraries
 
 #=============================================================================
-# Copyright 2014-2015 Kevin Funk <kfunk@kde.org>
+# Copyright 2014 Kevin Funk <kfunk@kde.org>
 #
 # Distributed under the OSI-approved BSD License (the "License");
 # see accompanying file Copyright.txt for details.
@@ -27,77 +26,85 @@
 
 #=============================================================================
 
-if (${Clang_FIND_REQUIRED})
-    find_package(LLVM ${Clang_FIND_VERSION} REQUIRED)
-else ()
-    find_package(LLVM ${Clang_FIND_VERSION})
-endif ()
 
-set(CLANG_FOUND FALSE)
+# WARNING: This file was copied from kdevelop/languages/clang/cmake/FindClang.cmake
+# submit any patches there instead
 
-if (LLVM_FOUND AND LLVM_LIBRARY_DIR)
-  macro(FIND_AND_ADD_CLANG_LIB _libname_)
-    string(TOUPPER ${_libname_} _prettylibname_)
-    find_library(CLANG_${_prettylibname_}_LIB NAMES ${_libname_} HINTS ${LLVM_LIBRARY_DIR})
-    if(CLANG_${_prettylibname_}_LIB)
-      set(CLANG_LIBS ${CLANG_LIBS} ${CLANG_${_prettylibname_}_LIB})
-    endif()
-  endmacro(FIND_AND_ADD_CLANG_LIB)
-
-  # note: On Windows there's 'libclang.dll' instead of 'clang.dll' -> search for 'libclang', too
-  find_library(CLANG_LIBCLANG_LIB NAMES clang libclang HINTS ${LLVM_LIBRARY_DIR}) # LibClang: high-level C interface
-
-  FIND_AND_ADD_CLANG_LIB(clangFrontend)
-  FIND_AND_ADD_CLANG_LIB(clangDriver)
-  FIND_AND_ADD_CLANG_LIB(clangCodeGen)
-  FIND_AND_ADD_CLANG_LIB(clangSema)
-  FIND_AND_ADD_CLANG_LIB(clangChecker)
-  FIND_AND_ADD_CLANG_LIB(clangAnalysis)
-  FIND_AND_ADD_CLANG_LIB(clangRewriteFrontend)
-  FIND_AND_ADD_CLANG_LIB(clangRewrite)
-  FIND_AND_ADD_CLANG_LIB(clangAST)
-  FIND_AND_ADD_CLANG_LIB(clangParse)
-  FIND_AND_ADD_CLANG_LIB(clangLex)
-  FIND_AND_ADD_CLANG_LIB(clangBasic)
-  FIND_AND_ADD_CLANG_LIB(clangARCMigrate)
-  FIND_AND_ADD_CLANG_LIB(clangEdit)
-  FIND_AND_ADD_CLANG_LIB(clangFrontendTool)
-  FIND_AND_ADD_CLANG_LIB(clangRewrite)
-  FIND_AND_ADD_CLANG_LIB(clangSerialization)
-  FIND_AND_ADD_CLANG_LIB(clangTooling)
-  FIND_AND_ADD_CLANG_LIB(clangStaticAnalyzerCheckers)
-  FIND_AND_ADD_CLANG_LIB(clangStaticAnalyzerCore)
-  FIND_AND_ADD_CLANG_LIB(clangStaticAnalyzerFrontend)
-  FIND_AND_ADD_CLANG_LIB(clangSema)
-  FIND_AND_ADD_CLANG_LIB(clangRewriteCore)
+if(${Clang_FIND_REQUIRED})
+  set(LLVM_FIND_TYPE REQUIRED)
+else()
+  set(LLVM_FIND_TYPE OPTIONAL)
 endif()
 
+find_package(LLVM CONFIG ${LLVM_FIND_TYPE} QUIET)
+# if(NOT LLVM_FOUND)
+#   find_package(LLVM 3.4 CONFIG ${LLVM_FIND_TYPE} QUIET)
+# endif()
+# if(NOT LLVM_FOUND)
+#   find_package(LLVM 3.5 CONFIG ${LLVM_FIND_TYPE} QUIET)
+# endif()
+# if(NOT LLVM_FOUND)
+#   find_package(LLVM 3.6 CONFIG ${LLVM_FIND_TYPE} QUIET)
+# endif()
+
+set(Clang_FOUND FALSE)
+
+set(CLANG_LIBRARY_DIR ${LLVM_LIBRARY_DIRS})
+set(CLANG_INCLUDE_DIR ${LLVM_INCLUDE_DIRS})
+
+macro(FIND_AND_ADD_CLANG_LIB _libname_)
+  string(TOUPPER ${_libname_} _prettylibname_)
+  find_library(CLANG_${_prettylibname_}_LIB NAMES "clang${_libname_}" HINTS ${LLVM_LIBRARY_DIRS})
+  if(CLANG_${_prettylibname_}_LIB)
+    add_library(Clang::${_libname_} UNKNOWN IMPORTED)
+    set_property(TARGET Clang::${_libname_} PROPERTY IMPORTED_LOCATION "${CLANG_${_prettylibname_}_LIB}")
+    set_property(TARGET Clang::${_libname_} PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${CLANG_INCLUDE_DIR})
+
+    set(CLANG_LIBS ${CLANG_LIBS} ${CLANG_${_prettylibname_}_LIB})
+  endif()
+endmacro(FIND_AND_ADD_CLANG_LIB)
+
+FIND_AND_ADD_CLANG_LIB(Frontend)
+FIND_AND_ADD_CLANG_LIB(Driver)
+FIND_AND_ADD_CLANG_LIB(CodeGen)
+FIND_AND_ADD_CLANG_LIB(Sema)
+FIND_AND_ADD_CLANG_LIB(Checker)
+FIND_AND_ADD_CLANG_LIB(Analysis)
+FIND_AND_ADD_CLANG_LIB(Rewrite)
+FIND_AND_ADD_CLANG_LIB(AST)
+FIND_AND_ADD_CLANG_LIB(Parse)
+FIND_AND_ADD_CLANG_LIB(Lex)
+FIND_AND_ADD_CLANG_LIB(Basic)
+FIND_AND_ADD_CLANG_LIB(ARCMigrate)
+FIND_AND_ADD_CLANG_LIB(Edit)
+FIND_AND_ADD_CLANG_LIB(FrontendTool)
+FIND_AND_ADD_CLANG_LIB(Serialization)
+FIND_AND_ADD_CLANG_LIB(Tooling)
+FIND_AND_ADD_CLANG_LIB(ToolingCore)
+FIND_AND_ADD_CLANG_LIB(StaticAnalyzerCheckers)
+FIND_AND_ADD_CLANG_LIB(StaticAnalyzerCore)
+FIND_AND_ADD_CLANG_LIB(StaticAnalyzerFrontend)
+FIND_AND_ADD_CLANG_LIB(RewriteCore)
+FIND_AND_ADD_CLANG_LIB(ASTMatchers)
+
 if(CLANG_LIBS)
-  set(CLANG_FOUND TRUE)
+  set(Clang_FOUND TRUE)
+  set(Clang_VERSION ${LLVM_VERSION})
+
 else()
   message(STATUS "Could not find any Clang libraries in ${LLVM_LIBRARY_DIR}")
 endif()
 
-if(CLANG_FOUND)
-  set(CLANG_LIBRARY_DIR ${LLVM_LIBRARY_DIR})
-  set(CLANG_INCLUDE_DIR ${LLVM_INCLUDE_DIR})
-
-  # check whether llvm-config comes from an install prefix
-  execute_process(
-    COMMAND ${LLVM_CONFIG_EXECUTABLE} --src-root
-    OUTPUT_VARIABLE _llvmSourceRoot
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
-  string(FIND "${LLVM_INCLUDE_DIR}" "${_llvmSourceRoot}" _llvmIsInstalled)
-  if (NOT _llvmIsInstalled)
-    message(STATUS "Detected that llvm-config comes from a build-tree, adding includes from source dir")
-    list(APPEND CLANG_INCLUDE_DIR "${_llvmSourceRoot}/tools/clang/include")
-  endif()
-
+if(Clang_FOUND)
   message(STATUS "Found Clang (LLVM version: ${LLVM_VERSION})")
-  message(STATUS "  Include dirs:       ${CLANG_INCLUDE_DIR}")
-  message(STATUS "  Clang libraries:    ${CLANG_LIBS}")
-  message(STATUS "  Libclang C library: ${CLANG_LIBCLANG_LIB}")
+  message(STATUS "  Include dirs:  ${CLANG_INCLUDE_DIR}")
+  message(STATUS "  Library dir:  ${CLANG_LIBRARY_DIR}")
+  message(STATUS "  Libraries:     ${CLANG_LIBS}")
+
+  string(REPLACE "." ";" LLVM_VERSION_LIST ${LLVM_VERSION})
+  list(GET LLVM_VERSION_LIST 0 LLVM_VERSION_MAJOR)
+  list(GET LLVM_VERSION_LIST 1 LLVM_VERSION_MINOR)
+  list(GET LLVM_VERSION_LIST 2 LLVM_VERSION_PATCH)
 else()
   if(Clang_FIND_REQUIRED)
     message(FATAL_ERROR "Could NOT find Clang")
