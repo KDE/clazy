f957510759bc5cab1542909aa5e3fcb912b3e2c7
Sergio Martins <smartins@kde.org>
2016-01-24 01:31:33 +0000

    Move inefficient-qlist to hidden level instead of level3
    level3 is for checks with lots of false positives, this check
    doesn't have false positives, it's just emits warnings we can't fix
    due to source and binary compat.
    
    We now have inefficient-qlist-soft which doesn't have source and binary
    compat problems

--- checks/inefficientqlist.cpp
+++ checks/inefficientqlist.cpp
@@ -41,4 +41,4 @@ InefficientQList::InefficientQList(const std::string &name, const clang::Compile
 {
 }
 
-REGISTER_CHECK_WITH_FLAGS("inefficient-qlist", InefficientQList, CheckLevel3)
+REGISTER_CHECK_WITH_FLAGS("inefficient-qlist", InefficientQList, HiddenCheckLevel)
