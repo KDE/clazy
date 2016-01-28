88bdd8773af135e4a8955636551904beb2f99baa
Sergio Martins <smartins@kde.org>
2016-01-28 23:17:40 +0000

    Forgot to commit the file ContextUtils.h/cpp

---
+++ ContextUtils.cpp
@@ -0,0 +1,194 @@
+/*
+  This file is part of the clazy static checker.
+
+  Copyright (C) 2016 Sergio Martins <smartins@kde.org>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
+*/
+
+#include "ContextUtils.h"
+#include "clazy_stl.h"
+#include "Utils.h"
+#include <clang/Basic/SourceLocation.h>
+#include <clang/Basic/SourceManager.h>
+#include <clang/AST/DeclCXX.h>
+#include <clang/AST/DeclFriend.h>
+
+using namespace clang;
+using namespace std;
+
+bool ContextUtils::isValueDeclInFunctionContext(clang::ValueDecl *valueDecl)
+{
+    DeclContext *context = valueDecl ? valueDecl->getDeclContext() : nullptr;
+    return context && isa<FunctionDecl>(context) && !isa<ParmVarDecl>(valueDecl);
+}
+
+std::vector<DeclContext *> ContextUtils::contextsForDecl(DeclContext *currentScope)
+{
+    std::vector<DeclContext *> decls;
+    decls.reserve(20); // jump-start
+    while (currentScope) {
+        decls.push_back(currentScope);
+        currentScope = currentScope->getParent();
+    }
+
+    return decls;
+}
+
+static string nameForContext(DeclContext *context)
+{
+    if (auto  *ns = dyn_cast<NamespaceDecl>(context)) {
+        return ns->getNameAsString();
+    } else if (auto rec = dyn_cast<CXXRecordDecl>(context)) {
+        return rec->getNameAsString();
+    } else if (auto *method = dyn_cast<CXXMethodDecl>(context)) {
+        return method->getNameAsString();
+    } else if (dyn_cast<TranslationUnitDecl>(context)){
+        return {};
+    } else {
+        llvm::errs() << "Unhandled kind: " << context->getDeclKindName() << "\n";
+    }
+
+    return {};
+}
+
+string ContextUtils::getMostNeededQualifiedName(const SourceManager &sourceManager,
+                                                CXXMethodDecl *method,
+                                                DeclContext *currentScope,
+                                                SourceLocation usageLoc, bool honourUsingDirectives)
+{
+    if (!currentScope)
+        return method->getQualifiedNameAsString();
+
+    // All namespaces, classes, inner class qualifications
+    auto methodContexts = ContextUtils::contextsForDecl(method->getDeclContext());
+
+    // Visible scopes in current scope
+    auto visibleContexts = ContextUtils::contextsForDecl(currentScope);
+
+    // Collect using directives
+    vector<UsingDirectiveDecl*> usings;
+    if (honourUsingDirectives) {
+        for (DeclContext *context : visibleContexts) {
+            auto range = context->using_directives();
+            for (auto it = range.begin(), end = range.end(); it != end; ++it) {
+                usings.push_back(*it);
+            }
+        }
+    }
+
+    for (UsingDirectiveDecl *u : usings) {
+        NamespaceDecl *ns = u->getNominatedNamespace();
+        if (ns) {
+            if (sourceManager.isBeforeInSLocAddrSpace(usageLoc, u->getLocStart()))
+                continue;
+
+            visibleContexts.push_back(ns->getOriginalNamespace());
+        }
+    }
+
+    for (DeclContext *context : visibleContexts) {
+
+        if (context != method->getParent()) { // Don't remove the most immediate
+            auto it = clazy_std::find_if(methodContexts, [context](DeclContext *c) {
+                    if (c == context)
+                        return true;
+                    auto ns1 = dyn_cast<NamespaceDecl>(c);
+                    auto ns2 = dyn_cast<NamespaceDecl>(context);
+                    return ns1 && ns2 && ns1->getQualifiedNameAsString() == ns2->getQualifiedNameAsString();
+
+                });
+            if (it != methodContexts.end()) {
+                methodContexts.erase(it, it + 1);
+            }
+        }
+    }
+
+    string neededContexts;
+    for (DeclContext *context : methodContexts) {
+        neededContexts = nameForContext(context) + "::" + neededContexts;
+    }
+
+    const string result = neededContexts + method->getNameAsString();
+    return result;
+}
+
+bool ContextUtils::canTakeAddressOf(CXXMethodDecl *method, DeclContext *context, bool &isSpecialProtectedCase)
+{
+    isSpecialProtectedCase = false;
+    if (!method || !method->getParent())
+        return false;
+
+    if (method->getAccess() == clang::AccessSpecifier::AS_public)
+        return true;
+
+    if (!context)
+        return false;
+
+    CXXRecordDecl *contextRecord = nullptr;
+
+    do {
+        contextRecord = dyn_cast<CXXRecordDecl>(context);
+        context = context->getParent();
+    } while (contextRecord == nullptr && context);
+
+    if (!contextRecord) // If we're not inside a class method we can't take the address of a private/protected method
+        return false;
+
+    CXXRecordDecl *record = method->getParent();
+    if (record == contextRecord)
+        return true;
+
+    // We're inside a method belonging to a class (contextRecord).
+    // Is contextRecord a friend of record ? Lets check:
+
+    for (auto fr : record->friends()) {
+        TypeSourceInfo *si = fr->getFriendType();
+        if (si) {
+            const Type *t = si->getType().getTypePtrOrNull();
+            CXXRecordDecl *friendClass = t ? t->getAsCXXRecordDecl() : nullptr;
+            if (friendClass == contextRecord) {
+                return true;
+            }
+        }
+    }
+
+    // There's still hope, lets see if the context is nested inside the class we're trying to access
+    // Inner classes can access private members of outter classes.
+    DeclContext *it = contextRecord;
+    do {
+        it = it->getParent();
+        if (it == record)
+            return true;
+    } while (it);
+
+    if (method->getAccess() == clang::AccessSpecifier::AS_private)
+        return false;
+
+    if (method->getAccess() != clang::AccessSpecifier::AS_protected) // shouldnt happen, must be protected at this point.
+        return false;
+
+    // For protected there's still hope, since record might be a derived or base class
+    if (Utils::derivesFrom(record, contextRecord))
+        return true;
+
+    if (Utils::derivesFrom(contextRecord, record)) {
+        isSpecialProtectedCase = true;
+        return true;
+    }
+
+    return false;
+}
---
+++ ContextUtils.h
@@ -0,0 +1,102 @@
+/*
+  This file is part of the clazy static checker.
+
+  Copyright (C) 2016 Sergio Martins <smartins@kde.org>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
+*/
+
+#ifndef CLAZY_CONTEXT_UTILS_H
+#define CLAZY_CONTEXT_UTILS_H
+
+#include <clang/AST/DeclBase.h>
+#include <clang/AST/Decl.h>
+#include <clang/AST/DeclCXX.h>
+#include <clang/AST/Stmt.h>
+
+#include <vector>
+#include <string>
+
+namespace clang {
+class ValueDecl;
+class DeclContext;
+class SourceManager;
+class SourceLocation;
+class CXXMethodDecl;
+}
+
+namespace ContextUtils
+{
+
+/**
+ * Returns true if a decl is inside a function, instead of say a class field.
+ * This returns true if "QList<int> l;" is a local variable, instead of being a class field such
+ * as struct Foo { QList<int> l; }
+ */
+bool isValueDeclInFunctionContext(clang::ValueDecl *);
+
+/**
+ * Returns the list of scopes for a decl context (namespaces, classes, inner classes, etc)
+ * The inner ones are at the beginning of the list
+ */
+std::vector<clang::DeclContext *> contextsForDecl(clang::DeclContext *);
+
+
+/**
+ * Returns the first context of type T in which the specified context is in.
+ * Contexts are namespaces, classes, inner classes, functions, etc.
+
+ */
+template <typename T>
+T* firstContextOfType(clang::DeclContext *context)
+{
+    if (!context)
+        return nullptr;
+
+    if (llvm::isa<T>(context))
+        return llvm::cast<T>(context);
+
+    return ContextUtils::firstContextOfType<T>(context->getParent());
+}
+
+
+/**
+ * Returns fully/semi-fully qualified name for a method, but doesn't over-qualify with namespaces
+ * which we're already in.
+ *
+ * if currentScope == nullptr will return a fully qualified name
+ */
+
+std::string getMostNeededQualifiedName(const clang::SourceManager &sourceManager,
+                                       clang::CXXMethodDecl *method,
+                                       clang::DeclContext *currentScope,
+                                       clang::SourceLocation usageLoc, bool honourUsingDirectives);
+
+/**
+ * Returns true, if in a specific context, we can take the address of a method
+ * for example doing &ClassName::SomePrivateMember might not be possible if the member is private
+ * but might be possible if we're in a context which is friend of ClassName
+ * Or it might be protected but context is a derived class
+ *
+ * When inside a derived class scope it's possible to take the address of a protected base method
+ * but only if you qualify it with the derived class name, so &Derived::baseMethod, instead of &Base::baseMethod
+ * If this was the case then isSpecialProtectedCase will be true
+ */
+bool canTakeAddressOf(clang::CXXMethodDecl *method, clang::DeclContext *context, bool &isSpecialProtectedCase);
+
+}
+
+#endif
