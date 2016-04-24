5db3f94fcb673a86a2e73944f2716bf4b0303eb9
Sergio Martins <smartins@kde.org>
2016-04-24 23:02:40 +0100

    Add ubuntu 16.04 / clang 3.8 docker test case

--- dev-scripts/test_build_on_distros.sh
+++ dev-scripts/test_build_on_distros.sh
@@ -9,5 +9,6 @@ fi
 
 docker run -i -t iamsergio/clazy-ubuntu-14.04 sh $BUILD_SCRIPT $BRANCH $J_FLAG && \
 docker run -i -t iamsergio/clazy-ubuntu-15.10 sh $BUILD_SCRIPT $BRANCH $J_FLAG && \
+docker run -i -t iamsergio/clazy-ubuntu-16.04 sh $BUILD_SCRIPT $BRANCH $J_FLAG && \
 docker run -i -t iamsergio/clazy-opensuse-tumbleweed sh $BUILD_SCRIPT $BRANCH $J_FLAG && \
 docker run -i -t iamsergio/clazy-archlinux sh $BUILD_SCRIPT $BRANCH $J_FLAG
