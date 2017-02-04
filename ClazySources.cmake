4aca981a3ece18de7b2eec69ca62909352c8d02f
Sergio Martins <iamsergio@gmail.com>
2017-02-04 20:11:46 +0000

    Also build old-style-connect check when on MSVC
    It works fine

--- ClazySources.cmake
+++ ClazySources.cmake
@@ -77,3 +77,5 @@ set(CLAZY_SRCS
   ${CMAKE_CURRENT_LIST_DIR}/checks/ruleofbase.cpp
   ${CMAKE_CURRENT_LIST_DIR}/Clazy.cpp
 )
+
+set(CLAZY_SRCS ${CLAZY_SRCS}  ${CMAKE_CURRENT_LIST_DIR}/checks/level2/oldstyleconnect.cpp)
