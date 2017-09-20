55e28db5a89dda34cf60227efe69a396e406b9d5
Sergio Martins <smartins@kde.org>
2017-09-20 23:03:04 +0100

    connect-non-signal: Unit-tests++

--- tests/connect-non-signal/main.cpp
+++ tests/connect-non-signal/main.cpp
@@ -9,10 +9,10 @@ public:
     int bar() { return 1; }
 signals:
     void mySig();
+    virtual void myVirtualSig();
 public Q_SLOTS:
     void mySlot();
 };
-
 void test()
 {
     auto *o = new MyObj();
@@ -36,4 +36,18 @@ void test2()
     o2->connect(o2, &MyObj2::mySig, o2, &MyObj2::foo); // OK
     QObject::connect(o2, &MyObj2::mySig, o2, &MyObj2::foo); // OK
     QObject::connect(o2, &MyObj2::foo, o2, &MyObj2::foo); // OK
+}
+
+class MyDerivedObj : public MyObj
+{
+    Q_OBJECT
+public:
+    void myVirtualSig() override;
+};
+
+void test3()
+{
+    auto *o = new MyDerivedObj();
+    QObject::connect(o, &MyDerivedObj::myVirtualSig, o, &MyObj::foo); // Warn, overriden but not a signal now
+    QObject::connect(o, &MyObj::myVirtualSig, o, &MyObj::foo); // OK
 }
