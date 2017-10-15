fffd420589d987821d31a06ea1483cf7fb8390f2
Sergio Martins <smartins@kde.org>
2017-10-15 20:27:25 +0100

    Add missing \n in debug message

--- src/QtUtils.cpp
+++ src/QtUtils.cpp
@@ -271,7 +271,7 @@ CXXMethodDecl *QtUtils::pmfFromConnect(CallExpr *funcCall, int argIndex)
     }
 
     if (argIndex >= numArgs) {
-        llvm::errs() << "error, invalid argIndex " << argIndex << " " << numArgs;
+        llvm::errs() << "error, invalid argIndex " << argIndex << " " << numArgs << "\n";
         return nullptr;
     }
 
