38945f7c90e63ef105ea629b3779e8fa6eb0c371
Sergio Martins <smartins@kde.org>
2018-01-17 17:34:13 +0000

    Fix clazy.bat when it's in a path with spaces
    Needs some quotes, thanks thomas!

--- windows-package/clazy-cl.bat
+++ windows-package/clazy-cl.bat
@@ -1,2 +1,2 @@
 @echo off
-%~dp0\clang\clang.exe --driver-mode=cl -Qunused-arguments -Xclang -load -Xclang ClangLazy.dll -Xclang -add-plugin -Xclang clang-lazy -Wno-microsoft-enum-value %*
+"%~dp0\clang\clang.exe" --driver-mode=cl -Qunused-arguments -Xclang -load -Xclang ClangLazy.dll -Xclang -add-plugin -Xclang clang-lazy -Wno-microsoft-enum-value %*
