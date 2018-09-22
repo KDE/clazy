c42c50cb0cbdfd722c62b710aee22e871ca68343
Sergio Martins <smartins@kde.org>
2018-09-22 23:12:51 +0100

    tests: Don't run qcolor-from-literal when using the crash workaround
    This check requires AST matches, which are disabled on debian+llvm7

--- tests/docker/build-clazy.sh
+++ tests/docker/build-clazy.sh
@@ -3,7 +3,8 @@
 BRANCH=$1
 J_FLAG=$2
 CLAZY_PREFIX=$3
-LLVM_ROOT=$3
+IGNORE_CHECKS=$4
+LLVM_ROOT = $5
 
 if [ -z "$1" ]
 then
@@ -20,7 +21,19 @@ then
     exit 1;
 fi
 
+if [ "$IGNORE_CHECKS" = "none" ]
+then
+    IGNORE_CHECKS = ""
+else
+    IGNORE_CHECKS = " --exclude ${IGNORE_CHECKS} "
+fi
+
+if [ "$5" = "none" ]
+then
+    unset LLVM_ROOT
+fi
+
 export PATH=$CLAZY_PREFIX/bin:$PATH
 export LD_LIBRARY_PATH=$CLAZY_PREFIX/lib:$CLAZY_PREFIX/lib64:$LD_LIBRARY_PATH
 
-cd /root/clazy && git fetch && git checkout origin/$BRANCH && cmake -DCMAKE_INSTALL_PREFIX=$CLAZY_PREFIX -DCMAKE_BUILD_TYPE=RelWithDebInfo . && make $J_FLAG && make install && cd tests && ./run_tests.py
+cd /root/clazy && git fetch && git checkout origin/$BRANCH && cmake -DCMAKE_INSTALL_PREFIX=$CLAZY_PREFIX -DCMAKE_BUILD_TYPE=RelWithDebInfo . && make $J_FLAG && make install && cd tests && ./run_tests.py $IGNORE_CHECKS
