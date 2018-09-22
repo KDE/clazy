3665a54a9f52dab0dcc0546145dfda72b6a1cb92
Sergio Martins <smartins@kde.org>
2018-09-22 18:59:22 +0100

    build-clazy.sh: Don't checkout an old branch
    Now we checkout what we fetched

--- tests/docker/build-clazy.sh
+++ tests/docker/build-clazy.sh
@@ -23,4 +23,4 @@ fi
 export PATH=$CLAZY_PREFIX/bin:$PATH
 export LD_LIBRARY_PATH=$CLAZY_PREFIX/lib:$CLAZY_PREFIX/lib64:$LD_LIBRARY_PATH
 
-cd /root/clazy && git pull && git checkout $BRANCH && cmake -DCMAKE_INSTALL_PREFIX=$CLAZY_PREFIX -DCMAKE_BUILD_TYPE=RelWithDebInfo . && make $J_FLAG && make install && cd tests && ./run_tests.py
+cd /root/clazy && git fetch && git checkout origin/$BRANCH && cmake -DCMAKE_INSTALL_PREFIX=$CLAZY_PREFIX -DCMAKE_BUILD_TYPE=RelWithDebInfo . && make $J_FLAG && make install && cd tests && ./run_tests.py
