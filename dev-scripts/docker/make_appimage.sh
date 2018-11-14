8da03474e540fbb415f129f108cfbec9e2452b5e
Sergio Martins <smartins@kde.org>
2018-11-14 23:10:46 +0000

    Print what the docker script is doing

--- dev-scripts/docker/make_appimage.sh
+++ dev-scripts/docker/make_appimage.sh
@@ -9,7 +9,18 @@ cp -r /clazy.AppDir /tmp/clazy_work/
 cd /clazy
 git clean -fdx .
 git checkout .
+
+echo "Running git pull..."
 git pull
+
+echo "Checking out $1..."
 git checkout $1
+
+echo "Building..."
 cmake3 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/tmp/clazy_work/clazy.AppDir/usr . && make -j12 && make install
+
+echo "Fixing permissions..."
 chown -R $2 /tmp/clazy_work/clazy.AppDir/
+
+echo "Done"
+echo
