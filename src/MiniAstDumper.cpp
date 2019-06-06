849343a794b21e37fa97133c92267ddb4137086b
Sergio Martins <sergio.martins@kdab.com>
2019-06-06 21:43:46 +0100

    WIP

--- src/MiniAstDumper.cpp
+++ src/MiniAstDumper.cpp
@@ -25,6 +25,8 @@
 #include <clang/Frontend/CompilerInstance.h>
 #include <clang/Frontend/FrontendPluginRegistry.h>
 
+#include <fstream>
+
 using namespace clang;
 using namespace std;
 
@@ -37,23 +39,75 @@ bool MiniAstDumperASTAction::ParseArgs(const CompilerInstance &, const std::vect
     return true;
 }
 
-std::unique_ptr<ASTConsumer> MiniAstDumperASTAction::CreateASTConsumer(CompilerInstance &, llvm::StringRef)
+std::unique_ptr<ASTConsumer> MiniAstDumperASTAction::CreateASTConsumer(CompilerInstance &ci, llvm::StringRef)
 {
-    return std::unique_ptr<MiniASTDumperConsumer>(new MiniASTDumperConsumer());
+    return std::unique_ptr<MiniASTDumperConsumer>(new MiniASTDumperConsumer(ci));
 }
 
-MiniASTDumperConsumer::MiniASTDumperConsumer()
+MiniASTDumperConsumer::MiniASTDumperConsumer(CompilerInstance &ci)
+    : m_ci(ci)
 {
+    auto &sm = m_ci.getASTContext().getSourceManager();
+    const FileEntry *fileEntry = sm.getFileEntryForID(sm.getMainFileID());
+
+    llvm::errs() << "Found TU: " << fileEntry->getName() << "\n";
+
+    const std::string currentCppFile = fileEntry->getName();
+
+
+    cbor_encoder_init(&m_cborEncoder, m_cborBuf, sizeof(m_cborBuf), 0);
+    cbor_encoder_create_map(&m_cborEncoder, &m_cborRootMapEncoder, 2);
+    cbor_encode_text_stringz(&m_cborRootMapEncoder, "tu");
+    cbor_encode_text_stringz(&m_cborRootMapEncoder, currentCppFile.c_str());
+    cbor_encode_text_stringz(&m_cborRootMapEncoder, "stuff");
+    cbor_encoder_create_array(&m_cborRootMapEncoder, &m_cborStuffArray, CborIndefiniteLength);
+
+
+    //cbor_encode_boolean(&m_cborRootMapEncoder, some_value);
+    //
 }
 
 MiniASTDumperConsumer::~MiniASTDumperConsumer()
 {
+    cbor_encoder_close_container(&m_cborRootMapEncoder, &m_cborRootMapEncoder);
+    cbor_encoder_close_container(&m_cborEncoder, &m_cborStuffArray);
+
+    size_t size = cbor_encoder_get_buffer_size(&m_cborEncoder, m_cborBuf);
+
+    std::ofstream myFile ("data.bin", std::ios::out | ios::binary);
+    myFile.write(reinterpret_cast<char*>(m_cborBuf), long(size));
 }
 
 bool MiniASTDumperConsumer::VisitDecl(Decl *decl)
 {
-    if (auto rec = dyn_cast<CXXRecordDecl>(decl)) {
-        llvm::errs() << "Found record: " << rec->getQualifiedNameAsString() << "\n";
+    if (auto tsd = dyn_cast<ClassTemplateSpecializationDecl>(decl)) {
+        llvm::errs() << "ClassTemplateSpecializationDecl: "  + tsd->getQualifiedNameAsString() + "\n";
+    } else if (auto rec = dyn_cast<CXXRecordDecl>(decl)) {
+        if (!rec->isThisDeclarationADefinition()) {
+            // No forward-declarations
+            return true;
+        }
+
+        if (rec->getDescribedClassTemplate()) {
+            // This is a template. We'll rather print it's specializations when catching ClassTemplateDecl
+            return true;
+        }
+        llvm::errs() << "Found record: " << rec->getQualifiedNameAsString()
+                     << "; this=" << rec
+                     << "\n";
+    } else if (auto ctd = dyn_cast<ClassTemplateDecl>(decl)) {
+        llvm::errs() << "Found template: " << ctd->getNameAsString()
+                     << "; this=" << ctd
+                     << "\n";
+        for (auto s : ctd->specializations()) {
+            llvm::errs() << "Found specialization: " << s->getQualifiedNameAsString() << "\n";
+            auto &args = s->getTemplateArgs();
+            const unsigned int count = args.size();
+            for (unsigned int i = 0; i < count; ++i) {
+                args.get(i).print(PrintingPolicy({}), llvm::errs());
+                llvm::errs() << "\n";
+            }
+        }
     }
 
     return true;
@@ -66,9 +120,6 @@ bool MiniASTDumperConsumer::VisitStmt(Stmt *)
 
 void MiniASTDumperConsumer::HandleTranslationUnit(ASTContext &ctx)
 {
-    auto &sm = ctx.getSourceManager();
-    const FileEntry *fileEntry = sm.getFileEntryForID(sm.getMainFileID());
-    llvm::errs() << "Found TU: " << fileEntry->getName() << "\n";
     TraverseDecl(ctx.getTranslationUnitDecl());
 }
 
