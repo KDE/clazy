88c9e7db99e71ee3cc10ac9f376e7dcce64065a1
Sergio Martins <iamsergio@gmail.com>
2020-08-28 12:02:01 +0100

    Added an HAS_STD_FILESYSTEM define
    As not all compilers supporting c++17 support it

---
+++ .cmake_has_filesystem_test.cpp
@@ -0,0 +1,28 @@
+/*
+   This file is part of the clazy static checker.
+
+  Copyright (C) 2015 Sergio Martins <smartins@kde.org>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
+*/
+
+#include <filesystem>
+
+int main()
+{
+    std::filesystem::path path;
+    return 0;
+}
