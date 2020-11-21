c1899b494199944dd6581dd181eee28d990f3288
Sergio Martins <smartins@kde.org>
2020-11-21 23:12:18 +0000

    Bump some missing versions to 1.8

--- dev-scripts/make_windows_package.py
+++ dev-scripts/make_windows_package.py
@@ -5,8 +5,8 @@ from shutil import copyfile
 
 #--------------------------------------------------
 # Change here:
-CLAZY_VERSION = '1.7'
-CANDIDATE_SHA1 = 'v1.7'
+CLAZY_VERSION = '1.8'
+CANDIDATE_SHA1 = 'v1.8'
 PACKAGE_DIR = '/d/clazy-msvc-package/'
 PACKAGE_DIR_WIN = 'd:\\clazy-msvc-package\\'
 #--------------------------------------------------
