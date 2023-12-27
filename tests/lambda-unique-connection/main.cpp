e9444c13a9e2096fcbc77d985dbb9a8cf60a7acc
David Faure <faure@kde.org>
2023-12-27 20:00:11 +0100

    Make lamba-unique-connection's tests closer to reality
    Calling the member connect() and then passing 5 arguments
    doesn't really make sense, we're using the static connect() method.
    
    (First step in my investigation of a false positive...)

--- tests/lambda-unique-connection/main.cpp
+++ tests/lambda-unique-connection/main.cpp
@@ -5,14 +5,14 @@ void globalFunc();
 void test()
 {
     QObject *o;
-    o->connect(o, &QObject::destroyed, o, []{}, Qt::UniqueConnection); // Warn
-    o->connect(o, &QObject::destroyed, o, []{}, Qt::ConnectionType(Qt::UniqueConnection | Qt::DirectConnection)); // Warn
-    o->connect(o, &QObject::destroyed, o, []{}); // OK
-    o->connect(o, &QObject::destroyed, o, globalFunc); // OK
     QObject::connect(o, &QObject::destroyed, o, []{}, Qt::UniqueConnection); // Warn
-    o->connect(o, &QObject::destroyed, o, globalFunc, Qt::UniqueConnection); // Warn
-    o->connect(o, &QObject::destroyed, o, &QObject::deleteLater, Qt::UniqueConnection); // OK
-    o->connect(o, QOverload<QObject*>::of(&QObject::destroyed), o, &QObject::deleteLater, Qt::UniqueConnection); // OK
-    o->connect(o, &QObject::destroyed, o, QOverload<>::of(&QObject::deleteLater), Qt::UniqueConnection); // OK
+    QObject::connect(o, &QObject::destroyed, o, []{}, Qt::ConnectionType(Qt::UniqueConnection | Qt::DirectConnection)); // Warn
+    QObject::connect(o, &QObject::destroyed, o, []{}); // OK
+    QObject::connect(o, &QObject::destroyed, o, globalFunc); // OK
+    QObject::connect(o, &QObject::destroyed, o, []{}, Qt::UniqueConnection); // Warn
+    QObject::connect(o, &QObject::destroyed, o, globalFunc, Qt::UniqueConnection); // Warn
+    QObject::connect(o, &QObject::destroyed, o, &QObject::deleteLater, Qt::UniqueConnection); // OK
+    QObject::connect(o, QOverload<QObject*>::of(&QObject::destroyed), o, &QObject::deleteLater, Qt::UniqueConnection); // OK
+    QObject::connect(o, &QObject::destroyed, o, QOverload<>::of(&QObject::deleteLater), Qt::UniqueConnection); // OK
 }
 
