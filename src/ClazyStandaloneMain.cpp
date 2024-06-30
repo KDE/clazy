56bf3b514cc90f8c8c5c11e74c9bdda71c6e1e9b
Sergio Martins <smartins@kde.org>
2024-06-30 18:00:14 +0100

    Don't hardcode version in ClazyStandaloneMain.cpp
    Use a define instead

--- src/ClazyStandaloneMain.cpp
+++ src/ClazyStandaloneMain.cpp
@@ -160,7 +160,7 @@ int main(int argc, const char **argv)
 {
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "--version") == 0) {
-            std::cout << "clazy version 1.12\n";
+            std::cout << "clazy version " << CLAZY_VERSION << "\n";
             break;
         }
     }
