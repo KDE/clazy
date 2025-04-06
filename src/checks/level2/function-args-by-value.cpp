2fd696115c43c8ca1e551c77d314e69e02b5aee6
Marcel Schneider <marcel@coopmasters.de>
2025-04-06 10:57:56 +0200

    [function-args-by-value] put parameter name into error

--- src/checks/level2/function-args-by-value.cpp
+++ src/checks/level2/function-args-by-value.cpp
@@ -191,8 +191,13 @@ void FunctionArgsByValue::processFunction(FunctionDecl *func)
                 }
             }
 
-            const std::string paramStr = param->getType().getAsString(lo());
-            std::string error = "Pass small and trivially-copyable type by value (" + paramStr + ')';
+            std::string paramStr = param->getType().getAsString(lo());
+            const std::string paramName = param->getNameAsString();
+
+            if (!paramName.empty())
+                paramStr.append(" ");
+            std::string funcName = func->getQualifiedNameAsString();
+            std::string error = funcName + ": Pass small and trivially-copyable type by value (" + paramStr + paramName + ')';
             emitWarning(param->getBeginLoc(), error, fixits);
         }
     }
