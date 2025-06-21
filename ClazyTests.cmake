5cf5c2019a2814b814f7e3ba98b9c1e4fb282256
Alexander Lohnau <alexander.lohnau@gmx.de>
2025-06-21 08:40:51 +0200

    Avoid puttiing to much static code in generated file

---
+++ ClazyTests.cmake
@@ -0,0 +1,25 @@
+enable_testing()
+
+if ($ENV{CI_JOB_NAME_SLUG} MATCHES "qt5")
+    set(TEST_VERSION_OPTION "--qt-versions=5")
+elseif($ENV{CI_JOB_NAME_SLUG} MATCHES "qt6")
+    set(TEST_VERSION_OPTION "--qt-versions=6")
+endif()
+if (NOT CLAZY_BUILD_CLANG_TIDY)
+    set(TEST_OPTIONS "--no-clang-tidy")
+endif()
+
+macro(add_clazy_test name)
+  add_test(NAME ${name} COMMAND python3 run_tests.py ${name} --verbose ${TEST_VERSION_OPTION} ${TEST_OPTIONS} WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/tests/)
+  if (CLANG_EXECUTABLE_PATH)
+    set(CLANG_CXX_TEST "CLANGXX=${CLANG_EXECUTABLE_PATH};")
+  endif()
+  if (CLAZY_BUILD_CLANG_TIDY)
+    set(CLANGTIDYPLUGIN_CXX "CLANGTIDYPLUGIN_CXX=$<TARGET_FILE:ClazyClangTidy>;")
+  endif()
+  set_property(TEST ${name} PROPERTY
+    ENVIRONMENT "${CLANG_CXX_TEST}${CLANGTIDYPLUGIN_CXX}CLAZYPLUGIN_CXX=$<TARGET_FILE:ClazyPlugin>;CLAZYSTANDALONE_CXX=$<TARGET_FILE:clazy-standalone>;$<$<BOOL:${HAS_STD_FILESYSTEM}>:CLAZY_HAS_FILESYSTEM=>"
+  )
+endmacro()
+
+include(ClazyTests.generated.cmake)
