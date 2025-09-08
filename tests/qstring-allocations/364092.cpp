7b50fdc9e5cc39ce07ea1aff996b68253bed5e2f
Alexander Lohnau <alexander.lohnau@gmx.de>
2025-09-08 15:06:24 +0200

    Do not build clang-tidy plugin just when checking QT_NAMESPACE

--- tests/qstring-allocations/364092.cpp
+++ tests/qstring-allocations/364092.cpp
@@ -5,14 +5,3 @@ inline constexpr QLatin1String operator""_L1 (const char *str, std::size_t len)
      return QLatin1String(str, len);
 }
 
-void test364092()
-{
-    QString s = "F"_L1; // Warning
-}
-
-void dummy()
-{
-    // Add at least one thing that will trigger a fixit it, otherwise, without
-    // any fixit the file won't be generated and that breaks the test
-    QString s = "test";
-}
