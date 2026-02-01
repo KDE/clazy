f5e1d63d25c2de2c2e54d8c1e1480d814d0fca9d
Alexander Lohnau <alexander.lohnau@gmx.de>
2026-02-01 14:24:30 +0100

    Throw error if config.json of checks has no tests section

--- tests/testutils/checks.py
+++ tests/testutils/checks.py
@@ -25,83 +25,84 @@ def load_check_from_json(check_name: str, clang_version: int) -> Check:
     decoded = json.loads(contents)
     check_blacklist_platforms = []
 
+    tests = decoded['tests'] # OK to throe error if not exist
+    del decoded["tests"]
     for key, value in decoded.items():
-        if hasattr(check, key) and key != "tests":
+        if hasattr(check, key):
             setattr(check, key, value)
 
     if 'blacklist_platforms' in decoded:
         check_blacklist_platforms = decoded['blacklist_platforms']
 
-    if 'tests' in decoded:
-        for t in decoded['tests']:
-            test = Test(check)
-            test.blacklist_platforms = check_blacklist_platforms
+    for t in tests:
+        test = Test(check)
+        test.blacklist_platforms = check_blacklist_platforms
 
-            if 'filename' in t:
-                test.filenames.append(t['filename'])
+        if 'filename' in t:
+            test.filenames.append(t['filename'])
 
-            if 'filenames' in t:
-                test.filenames += t['filenames']
+        if 'filenames' in t:
+            test.filenames += t['filenames']
 
-            if 'minimum_qt_version' in t:
-                test.minimum_qt_version = t['minimum_qt_version']
-            else:
-                test.minimum_qt_version = check.minimum_qt_version
+        if 'minimum_qt_version' in t:
+            test.minimum_qt_version = t['minimum_qt_version']
+        else:
+            test.minimum_qt_version = check.minimum_qt_version
 
-            if 'maximum_qt_version' in t:
-                test.maximum_qt_version = t['maximum_qt_version']
-            else:
-                test.maximum_qt_version = check.maximum_qt_version
+        if 'maximum_qt_version' in t:
+            test.maximum_qt_version = t['maximum_qt_version']
+        else:
+            test.maximum_qt_version = check.maximum_qt_version
 
-            if 'minimum_clang_version' in t:
-                test.minimum_clang_version = t['minimum_clang_version']
-            else:
-                test.minimum_clang_version = check.minimum_clang_version
+        if 'minimum_clang_version' in t:
+            test.minimum_clang_version = t['minimum_clang_version']
+        else:
+            test.minimum_clang_version = check.minimum_clang_version
 
-            if 'minimum_clang_version_for_fixits' in t:
-                test.minimum_clang_version_for_fixits = t['minimum_clang_version_for_fixits']
+        if 'minimum_clang_version_for_fixits' in t:
+            test.minimum_clang_version_for_fixits = t['minimum_clang_version_for_fixits']
 
-            if 'blacklist_platforms' in t:
-                test.blacklist_platforms = t['blacklist_platforms']
-            if 'compare_everything' in t:
-                test.compare_everything = t['compare_everything']
-            if 'qt_major_versions' in t:
-                test.setQtMajorVersions(t['qt_major_versions'])
-            if 'env' in t:
-                test.setEnv(t['env'])
-            if 'checks' in t:
-                test.checks = t['checks']
-            if 'flags' in t:
-                test.flags = t['flags']
-            if 'must_fail' in t:
-                test.must_fail = t['must_fail']
-            if 'has_fixits' in t:
-                test.has_fixits = t['has_fixits'] and test.minimum_clang_version_for_fixits <= clang_version
-            if 'expects_failure' in t:
-                test.expects_failure = t['expects_failure']
-            if 'skip_qtnamespaced' in t:
-                test.skip_qtnamespaced = t['skip_qtnamespaced']
-            if 'only_qt' in t:
-                test.only_qt = t['only_qt']
-            if 'cppStandards' in t:
-                test.cppStandards = t['cppStandards']
-            if 'qt_developer' in t:
-                test.qt_developer = t['qt_developer']
-            if 'extra_definitions' in t:
-                test.extra_definitions = " " + t['extra_definitions'] + " "
-            if 'header_filter' in t:
-                test.header_filter = t['header_filter']
-            if 'ignore_dirs' in t:
-                test.ignore_dirs = t['ignore_dirs']
-            if 'should_run_on_32bit' in t:
-                test.should_run_on_32bit = t['should_run_on_32bit']
-            if 'qt_modules_includes' in t:
-                test.qt_modules_includes = t['qt_modules_includes']
+        if 'blacklist_platforms' in t:
+            test.blacklist_platforms = t['blacklist_platforms']
+        if 'compare_everything' in t:
+            test.compare_everything = t['compare_everything']
+        if 'qt_major_versions' in t:
+            test.setQtMajorVersions(t['qt_major_versions'])
+        if 'env' in t:
+            test.setEnv(t['env'])
+        if 'checks' in t:
+            test.checks = t['checks']
+        if 'flags' in t:
+            test.flags = t['flags']
+        if 'must_fail' in t:
+            test.must_fail = t['must_fail']
+        if 'has_fixits' in t:
+            test.has_fixits = t['has_fixits'] and test.minimum_clang_version_for_fixits <= clang_version
+        if 'expects_failure' in t:
+            test.expects_failure = t['expects_failure']
+        if 'skip_qtnamespaced' in t:
+            test.skip_qtnamespaced = t['skip_qtnamespaced']
+        if 'only_qt' in t:
+            test.only_qt = t['only_qt']
+        if 'cppStandards' in t:
+            test.cppStandards = t['cppStandards']
+        if 'qt_developer' in t:
+            test.qt_developer = t['qt_developer']
+        if 'extra_definitions' in t:
+            test.extra_definitions = " " + t['extra_definitions'] + " "
+        if 'header_filter' in t:
+            test.header_filter = t['header_filter']
+        if 'ignore_dirs' in t:
+            test.ignore_dirs = t['ignore_dirs']
+        if 'should_run_on_32bit' in t:
+            test.should_run_on_32bit = t['should_run_on_32bit']
+        if 'qt_modules_includes' in t:
+            test.qt_modules_includes = t['qt_modules_includes']
 
-            if not test.checks:
-                test.checks.append(test.check.name)
+        if not test.checks:
+            test.checks.append(test.check.name)
 
-            check.tests.append(test)
+        check.tests.append(test)
 
     return check
 
