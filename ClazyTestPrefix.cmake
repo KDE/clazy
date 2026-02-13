2c8771e75a3c8ae20aaf89b907de2a5a9cdd9750
Alexander Lohnau <alexander.lohnau@gmx.de>
2026-02-13 21:35:16 +0100

    Export ENV variables for clang/clang-tidy executable paths for test prefix script too
    Needed when compiling against the non-system clang

--- ClazyTestPrefix.cmake
+++ ClazyTestPrefix.cmake
@@ -11,6 +11,12 @@ export CLAZYSTANDALONE_CXX="$<TARGET_FILE:clazy-standalone>"
 if (CLAZY_BUILD_CLANG_TIDY)
   string(APPEND script_content "export CLANGTIDYPLUGIN_CXX=\"$<TARGET_FILE:ClazyClangTidy>\"\n")
 endif()
+if (CLANG_EXECUTABLE_PATH)
+  string(APPEND script_content "export CLANGXX=\"${CLANG_EXECUTABLE_PATH}\"\n")
+endif()
+if (CLANG_TIDY_EXECUTABLE_PATH)
+  string(APPEND script_content "export CLANGTIDY=\"${CLANG_TIDY_EXECUTABLE_PATH}\"\n")
+endif()
 
 file(GENERATE
   OUTPUT "${test_prefix}"
